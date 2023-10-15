8356cb794b47d9d8676594531675604c5c5eb9c1
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-10-14 22:00:30 -0400

    lint

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,9 +1,10 @@
-/* enlist-disable */
+/* eslint-disable */
+
 export default function getStudentsByLocation(students, city) {
-    if (!Array.isArray(students)) {
-      return [];
-    }
-  
-    const filter = students.filter((filteredObj) => filteredObj.location === city);
-    return filter;
+  if (!Array.isArray(students)) {
+    return [];
   }
+
+  const filter = students.filter((filteredObj) => filteredObj.location === city);
+  return filter;
+}
