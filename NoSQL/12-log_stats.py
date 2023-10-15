1065cf1addd000f34dee0c38e2f91c9427394426
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-10-15 00:08:15 -0400

    FIX

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,23 +2,27 @@
 """ Log stats """
 from pymongo import MongoClient
 
-
-if __name__ == "__main__":
-
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db_nginx = client.logs.nginx
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    count_logs = db_nginx.count_documents({})
+def print_logs_stats(collection):
+    # Total logs count
+    count_logs = collection.count_documents({})
     print(f'{count_logs} logs')
 
+    # Count and print each HTTP method
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print('Methods:')
     for method in methods:
-        count_method = db_nginx.count_documents({'method': method})
-        print(f'\tmethod {method}: {count_method}')
+        count_method = collection.count_documents({'method': method})
+        print(f'\tMethod {method}: {count_method}')
 
-    check = db_nginx.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-
+    # Count and print status checks
+    check = collection.count_documents({"method": "GET", "path": "/status"})
     print(f'{check} status check')
+
+if __name__ == "__main__":
+    try:
+        client = MongoClient('mongodb://127.0.0.1:27017')
+        db_nginx = client.logs.nginx
+        print_logs_stats(db_nginx)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
