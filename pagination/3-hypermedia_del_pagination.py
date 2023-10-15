82fda8a4754e9d0e68efac0ddad56a10ac9b93b1
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-10-15 00:02:57 -0400

    pycode

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,16 +1,9 @@
-#!/usr/bin/env python3
-"""
-Deletion-resilient hypermedia pagination
-"""
-
 import csv
-import math
 from typing import List, Dict
 
-
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+    
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -18,8 +11,7 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -29,19 +21,14 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
-        """
+        """Dataset indexed by sorting position, starting at 0."""
         if self.__indexed_dataset is None:
-            dataset = self.dataset()
-            truncated_dataset = dataset[:1000]
-            self.__indexed_dataset = {
-                i: dataset[i] for i in range(len(dataset))
-            }
+            dataset = self.dataset()[:1000]
+            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """method that get the hyper index"""
-
+        """Method that gets the hyper index."""
         result_dataset = []
         index_data = self.indexed_dataset()
         keys_list = list(index_data.keys())
@@ -71,6 +58,7 @@ class Server:
             'index': index,
             'next_index': next_index,
             'page_size': len(result_dataset),
-            'data': result_dataset}
+            'data': result_dataset
+        }
 
         return hyper_dict
