40257dd519659e3ce2e007d6b6d92911ea2b504f
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-10-14 22:21:44 -0400

    lint

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,11 @@
-/* eslin-disabled */
+/* eslint-disable */
 function handleResponseFromAPI(promise) {
-    const body = { status: 200, body: 'success' };
-  
-    return promise
-      .then(() => body)
-      .catch((error) => error)
-      .finally(() => console.log('Got a response from the API'));
-  }
-  
-  export default handleResponseFromAPI;
+  const body = { status: 200, body: 'success' };
+
+  return promise
+    .then(() => body)
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
+}
+
+export default handleResponseFromAPI;
